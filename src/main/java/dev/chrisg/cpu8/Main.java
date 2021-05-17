dbf2e9e48ca869ff87b62997ca62cf949db59d0f
Christopher Girvin <chrisg8691@gmail.com>
2021-05-17 17:49:29 -0400

    load working; also added trace method;

--- src/main/java/dev/chrisg/cpu8/Main.java
+++ src/main/java/dev/chrisg/cpu8/Main.java
@@ -3,7 +3,7 @@ package dev.chrisg.cpu8;
 public class Main {
     public static void main(String[] args) {
         CPU cpu = new CPU();
-        cpu.load("")
-        System.out.println(args.stream());
+        cpu.load("/home/chrisg/Documents/projects/java/8bit_CPU/src/main/java/dev/chrisg/cpu8/programs/print8.cpu8");
+        cpu.trace();
     }
 }
